0b64be5b6eac818638532c8ad60e1aa223fec648
CAOH1024 <1745648316@qq.com>
2025-06-06 08:56:22 +0800

    blink

--- main/gpio_example_main.c
+++ main/gpio_example_main.c
@@ -7,7 +7,14 @@
 void app_main(void)
 {
     // 配置 GPIO2 为输出模式
-    gpio_pad_select_gpio(BLINK_GPIO);
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << BLINK_GPIO),  // 设置GPIO2
+        .mode = GPIO_MODE_OUTPUT,              // 设置为输出模式
+        .pull_up_en = GPIO_PULLUP_DISABLE,     // 禁用上拉
+        .pull_down_en = GPIO_PULLDOWN_DISABLE, // 禁用下拉
+        .intr_type = GPIO_INTR_DISABLE         // 禁用中断
+    };
+    gpio_config(&io_conf);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
     while (1) {
@@ -20,3 +27,4 @@ void app_main(void)
         vTaskDelay(pdMS_TO_TICKS(500));  // 延时500毫秒
     }
 }
+ 
